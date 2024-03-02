a6089efbfde5f47ebafd64475c1f4109694508b1
kkevinsm <76790044+kkevinsm@users.noreply.github.com>
2024-03-02 10:32:55 +0700

    benerin pemilihimport

--- app/Imports/PemilihsImport.php
+++ app/Imports/PemilihsImport.php
@@ -4,30 +4,36 @@ namespace App\Imports;
 
 use App\Models\Pemilih;
 use App\Models\User;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToModel;
 
 class PemilihsImport implements ToModel
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * Generate a random string.
+     *
+     * @param int $length
+     * @return string
+     */
+    public static function generateRandomString($length = 4)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    /**
+     * @param array $row
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
-        function generateRandomString($length = 8) {
-            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            $charactersLength = strlen($characters);
-            $randomString = '';
-            for ($i = 0; $i < $length; $i++) {
-                $randomString .= $characters[random_int(0, $charactersLength - 1)];
-            }
-            return $randomString;
-        }
-
-        $username = generateRandomString(4);
-        $password = generateRandomString(4);
+        $username = self::generateRandomString(4);
+        $password = self::generateRandomString(4);
 
         User::create([
             'name' => $row[0],
